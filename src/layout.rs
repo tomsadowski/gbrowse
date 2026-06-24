974415130a076038a86022cd321b583c5e20626c
tomsadowski <tsadowski93@gmail.com>
2026-06-24 18:18:49 -0400

    i foreget

--- src/layout.rs
+++ src/layout.rs
@@ -1,7 +1,9 @@
 // src/layout.rs
 
 use crate::{
-  Rect, GetRect,
+  Rect, 
+  GetRect,
+  Frame,
   ScreenCursor,
 };
 use std::collections::HashMap;
@@ -12,27 +14,29 @@ pub const DLG_HEAD: u8 = 1;
 pub const DLG_BODY: u8 = 2;
 pub const MSG:      u8 = 3;
 
-pub struct Window {
-  cursor: ScreenCursor,
+pub struct View {
+  must_show: bool,
+  frame:     Option<Frame>,
+  cursor:    ScreenCursor,
 }
 
 pub struct Layout {
-  pub rect:     Rect,
-  pub overlays: HashMap<u8, Window>,
+  pub rect:  Rect,
+  pub views: HashMap<u8, View>,
 }
 
 impl<T: GetRect> From<&T> for Layout {
   fn from(t: &T) -> Self {
     Self {
-      rect:     t.get_rect(),
-      overlays: HashMap::default(),
+      rect:  t.get_rect(),
+      views: HashMap::default(),
     }
   }
 }
 
 impl Layout {
   // add
-  pub fn add(&mut self, handle: &str, show: bool) {
+  pub fn add(&mut self, handle: &str, view: View) {
   }
   // remove
   pub fn remove(&mut self, handle: &str) {
